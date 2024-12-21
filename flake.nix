7a09654b53042acfdf4ea245577cd3e5b424a3ab
rydnr <github@acm-sl.org>
2024-12-21 10:47:06 +0100

    Initial flake

---
+++ flake.nix
@@ -0,0 +1,295 @@
+# flake.nix
+#
+# This file packages pythoneda-tools-runtime/flow-inspector-infrastructure as a Nix flake.
+#
+# Copyright (C) 2024-today rydnr's pythoneda-tools-runtime/flow-inspector-infrastructure
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+{
+  description = "Nix flake for pythoneda-tools-runtime/flow-inspector-infrastructure";
+  inputs = rec {
+    flake-utils.url = "github:numtide/flake-utils/v1.0.0";
+    nixpkgs.url = "github:NixOS/nixpkgs/24.05";
+    pythoneda-shared-pythonlang-banner = {
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:pythoneda-shared-pythonlang-def/banner/0.0.74";
+    };
+    pythoneda-shared-pythonlang-domain = {
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.pythoneda-shared-pythonlang-banner.follows =
+        "pythoneda-shared-pythonlang-banner";
+      url = "github:pythoneda-shared-pythonlang-def/domain/0.0.105";
+    };
+    pythoneda-shared-pythonlang-infrastructure = {
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.pythoneda-shared-pythonlang-banner.follows =
+        "pythoneda-shared-pythonlang-banner";
+      url = "github:pythoneda-shared-pythonlang-def/infrastructure/0.0.81";
+    };
+    pythoneda-tools-runtime-flow-inspector = {
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.pythoneda-shared-pythonlang-banner.follows =
+        "pythoneda-shared-pythonlang-banner";
+      inputs.pythoneda-shared-pythonlang-domain.follows =
+        "pythoneda-shared-pythonlang-domain";
+      url = "github:pythoneda-tools-runtime-def/flow-inspector/0.0.1";
+    };
+  };
+  outputs = inputs:
+    with inputs;
+    let
+      defaultSystems = flake-utils.lib.defaultSystems;
+      supportedSystems = if builtins.elem "armv6l-linux" defaultSystems then
+        defaultSystems
+      else
+        defaultSystems ++ [ "armv6l-linux" ];
+    in flake-utils.lib.eachSystem supportedSystems (system:
+      let
+        org = "pythoneda-tools-runtime";
+        repo = "flow-inspector-infrastructure";
+        version = "0.0.1";
+        sha256 = "sha256-bv1M46zAAQbe9kstl8O7T6PwgN40OH3rPiJvSoEGLA0=";
+        pname = "${org}-${repo}";
+        pythonpackage = "pythoneda.tools.runtime.flow_inspector.infrastructure";
+        package = builtins.replaceStrings [ "." ] [ "/" ] pythonpackage;
+        description = "Infrastructure layer for pythoneda-tools-runtime/flow-inspector";
+        license = pkgs.lib.licenses.gpl3;
+        homepage = "https://github.com/pythoneda-tools-runtime/flow-inspector-infrastructure";
+        maintainers = [ "rydnr <github@acm-sl.org>" ];
+        archRole = "B";
+        space = "R";
+        layer = "I";
+        nixpkgsVersion = builtins.readFile "${nixpkgs}/.version";
+        nixpkgsRelease =
+          builtins.replaceStrings [ "\n" ] [ "" ] "nixpkgs-${nixpkgsVersion}";
+        shared = import "${pythoneda-shared-pythonlang-banner}/nix/shared.nix";
+        pkgs = import nixpkgs { inherit system; };
+        pythoneda-tools-runtime-flow-inspector-infrastructure-for = { python
+          , pythoneda-shared-pythonlang-banner
+          , pythoneda-shared-pythonlang-domain
+          , pythoneda-shared-pythonlang-infrastructure
+          , pythoneda-tools-runtime-flow-inspector
+          }:
+          let
+            pnameWithUnderscores =
+              builtins.replaceStrings [ "-" ] [ "_" ] pname;
+            pythonVersionParts = builtins.splitVersion python.version;
+            pythonMajorVersion = builtins.head pythonVersionParts;
+            pythonMajorMinorVersion =
+              "${pythonMajorVersion}.${builtins.elemAt pythonVersionParts 1}";
+            wheelName =
+              "${pnameWithUnderscores}-${version}-py${pythonMajorVersion}-none-any.whl";
+          in python.pkgs.buildPythonPackage rec {
+            inherit pname version;
+            projectDir = ./.;
+            pyprojectTomlTemplate = ./templates/pyproject.toml.template;
+            pyprojectToml = pkgs.substituteAll {
+              authors = builtins.concatStringsSep ","
+                (map (item: ''"${item}"'') maintainers);
+              desc = description;
+              inherit homepage pname pythonMajorMinorVersion package
+                version;
+              pythonedaSharedPythonlangDomain =
+                pythoneda-shared-pythonlang-domain.version;
+              pythonedaSharedPythonlangInfrastructure =
+                pythoneda-shared-pythonlang-infrastructure.version;
+              pythonedaToolsRuntimeFlowInspector =
+                pythoneda-tools-runtime-flow-inspector.version;
+              src = pyprojectTomlTemplate;
+            };
+
+            src = pkgs.fetchFromGitHub {
+              owner = org;
+              rev = version;
+              inherit repo sha256;
+            };
+
+            format = "pyproject";
+
+            nativeBuildInputs = with python.pkgs; [ pip poetry-core ];
+            propagatedBuildInputs = with python.pkgs; [
+              pythoneda-shared-pythonlang-domain
+              pythoneda-shared-pythonlang-infrastructure
+              pythoneda-tools-runtime-flow-inspector
+            ];
+
+            # pythonImportsCheck = [ pythonpackage ];
+
+            unpackPhase = ''
+              cp -r ${src} .
+              sourceRoot=$(ls | grep -v env-vars)
+              find $sourceRoot -type d -exec chmod 777 {} \;
+              cp ${pyprojectToml} $sourceRoot/pyproject.toml
+            '';
+
+            postInstall = ''
+              pushd /build/$sourceRoot
+              for f in $(find . -name '__init__.py'); do
+                if [[ ! -e $out/lib/python${pythonMajorMinorVersion}/site-packages/$f ]]; then
+                  cp $f $out/lib/python${pythonMajorMinorVersion}/site-packages/$f;
+                fi
+              done
+              popd
+              mkdir $out/dist
+              cp dist/${wheelName} $out/dist
+            '';
+
+            meta = with pkgs.lib; {
+              inherit description homepage license maintainers;
+            };
+          };
+      in rec {
+        defaultPackage = packages.default;
+        devShells = rec {
+          default = pythoneda-tools-runtime-flow-inspector-infrastructure-python312;
+          pythoneda-tools-runtime-flow-inspector-infrastructure-python39 = shared.devShell-for {
+            banner = "${
+                pythoneda-shared-pythonlang-banner.packages.${system}.pythoneda-shared-pythonlang-banner-python39
+              }/bin/banner.sh";
+            extra-namespaces = "";
+            nixpkgs-release = nixpkgsRelease;
+            package = packages.pythoneda-tools-runtime-flow-inspector-infrastructure-python39;
+            python = pkgs.python39;
+            pythoneda-shared-pythonlang-banner =
+              pythoneda-shared-pythonlang-banner.packages.${system}.pythoneda-shared-pythonlang-banner-python39;
+            pythoneda-shared-pythonlang-domain =
+              pythoneda-shared-pythonlang-domain.packages.${system}.pythoneda-shared-pythonlang-domain-python39;
+            inherit archRole layer org pkgs repo space;
+          };
+          pythoneda-tools-runtime-flow-inspector-infrastructure-python310 = shared.devShell-for {
+            banner = "${
+                pythoneda-shared-pythonlang-banner.packages.${system}.pythoneda-shared-pythonlang-banner-python310
+              }/bin/banner.sh";
+            extra-namespaces = "";
+            nixpkgs-release = nixpkgsRelease;
+            package = packages.pythoneda-tools-runtime-flow-inspector-infrastructure-python310;
+            python = pkgs.python310;
+            pythoneda-shared-pythonlang-banner =
+              pythoneda-shared-pythonlang-banner.packages.${system}.pythoneda-shared-pythonlang-banner-python310;
+            pythoneda-shared-pythonlang-domain =
+              pythoneda-shared-pythonlang-domain.packages.${system}.pythoneda-shared-pythonlang-domain-python310;
+            inherit archRole layer org pkgs repo space;
+          };
+          pythoneda-tools-runtime-flow-inspector-infrastructure-python311 = shared.devShell-for {
+            banner = "${
+                pythoneda-shared-pythonlang-banner.packages.${system}.pythoneda-shared-pythonlang-banner-python311
+              }/bin/banner.sh";
+            extra-namespaces = "";
+            nixpkgs-release = nixpkgsRelease;
+            package = packages.pythoneda-tools-runtime-flow-inspector-infrastructure-python311;
+            python = pkgs.python311;
+            pythoneda-shared-pythonlang-banner =
+              pythoneda-shared-pythonlang-banner.packages.${system}.pythoneda-shared-pythonlang-banner-python311;
+            pythoneda-shared-pythonlang-domain =
+              pythoneda-shared-pythonlang-domain.packages.${system}.pythoneda-shared-pythonlang-domain-python311;
+            inherit archRole layer org pkgs repo space;
+          };
+          pythoneda-tools-runtime-flow-inspector-infrastructure-python312 = shared.devShell-for {
+            banner = "${
+                pythoneda-shared-pythonlang-banner.packages.${system}.pythoneda-shared-pythonlang-banner-python312
+              }/bin/banner.sh";
+            extra-namespaces = "";
+            nixpkgs-release = nixpkgsRelease;
+            package = packages.pythoneda-tools-runtime-flow-inspector-infrastructure-python312;
+            python = pkgs.python312;
+            pythoneda-shared-pythonlang-banner =
+              pythoneda-shared-pythonlang-banner.packages.${system}.pythoneda-shared-pythonlang-banner-python312;
+            pythoneda-shared-pythonlang-domain =
+              pythoneda-shared-pythonlang-domain.packages.${system}.pythoneda-shared-pythonlang-domain-python312;
+            inherit archRole layer org pkgs repo space;
+          };
+          pythoneda-tools-runtime-flow-inspector-infrastructure-python313 = shared.devShell-for {
+            banner = "${
+                pythoneda-shared-pythonlang-banner.packages.${system}.pythoneda-shared-pythonlang-banner-python313
+              }/bin/banner.sh";
+            extra-namespaces = "";
+            nixpkgs-release = nixpkgsRelease;
+            package = packages.pythoneda-tools-runtime-flow-inspector-infrastructure-python313;
+            python = pkgs.python313;
+            pythoneda-shared-pythonlang-banner =
+              pythoneda-shared-pythonlang-banner.packages.${system}.pythoneda-shared-pythonlang-banner-python313;
+            pythoneda-shared-pythonlang-domain =
+              pythoneda-shared-pythonlang-domain.packages.${system}.pythoneda-shared-pythonlang-domain-python313;
+            inherit archRole layer org pkgs repo space;
+          };
+        };
+        packages = rec {
+          default = pythoneda-tools-runtime-flow-inspector-infrastructure-python312;
+          pythoneda-tools-runtime-flow-inspector-infrastructure-python39 =
+            pythoneda-tools-runtime-flow-inspector-infrastructure-for {
+              python = pkgs.python39;
+              pythoneda-shared-pythonlang-banner =
+                pythoneda-shared-pythonlang-banner.packages.${system}.pythoneda-shared-pythonlang-banner-python39;
+              pythoneda-shared-pythonlang-domain =
+                pythoneda-shared-pythonlang-domain.packages.${system}.pythoneda-shared-pythonlang-domain-python39;
+              pythoneda-shared-pythonlang-infrastructure =
+                pythoneda-shared-pythonlang-infrastructure.packages.${system}.pythoneda-shared-pythonlang-infrastructure-python39;
+              pythoneda-tools-runtime-flow-inspector =
+                pythoneda-tools-runtime-flow-inspector.packages.${system}.pythoneda-tools-runtime-flow-inspector-python39;
+            };
+          pythoneda-tools-runtime-flow-inspector-infrastructure-python310 =
+            pythoneda-tools-runtime-flow-inspector-infrastructure-for {
+              python = pkgs.python310;
+              pythoneda-shared-pythonlang-banner =
+                pythoneda-shared-pythonlang-banner.packages.${system}.pythoneda-shared-pythonlang-banner-python310;
+              pythoneda-shared-pythonlang-domain =
+                pythoneda-shared-pythonlang-domain.packages.${system}.pythoneda-shared-pythonlang-domain-python310;
+              pythoneda-shared-pythonlang-infrastructure =
+                pythoneda-shared-pythonlang-infrastructure.packages.${system}.pythoneda-shared-pythonlang-infrastructure-python310;
+              pythoneda-tools-runtime-flow-inspector =
+                pythoneda-tools-runtime-flow-inspector.packages.${system}.pythoneda-tools-runtime-flow-inspector-python310;
+            };
+          pythoneda-tools-runtime-flow-inspector-infrastructure-python311 =
+            pythoneda-tools-runtime-flow-inspector-infrastructure-for {
+              python = pkgs.python311;
+              pythoneda-shared-pythonlang-application =
+                pythoneda-shared-pythonlang-banner.packages.${system}.pythoneda-shared-pythonlang-banner-python311;
+              pythoneda-shared-pythonlang-domain =
+                pythoneda-shared-pythonlang-domain.packages.${system}.pythoneda-shared-pythonlang-domain-python311;
+              pythoneda-shared-pythonlang-infrastructure =
+                pythoneda-shared-pythonlang-infrastructure.packages.${system}.pythoneda-shared-pythonlang-infrastructure-python311;
+              pythoneda-tools-runtime-flow-inspector =
+                pythoneda-tools-runtime-flow-inspector.packages.${system}.pythoneda-tools-runtime-flow-inspector-python311;
+            };
+          pythoneda-tools-runtime-flow-inspector-infrastructure-python312 =
+            pythoneda-tools-runtime-flow-inspector-infrastructure-for {
+              python = pkgs.python312;
+              pythoneda-shared-pythonlang-banner =
+                pythoneda-shared-pythonlang-banner.packages.${system}.pythoneda-shared-pythonlang-banner-python312;
+              pythoneda-shared-pythonlang-domain =
+                pythoneda-shared-pythonlang-domain.packages.${system}.pythoneda-shared-pythonlang-domain-python312;
+              pythoneda-shared-pythonlang-infrastructure =
+                pythoneda-shared-pythonlang-infrastructure.packages.${system}.pythoneda-shared-pythonlang-infrastructure-python312;
+              pythoneda-tools-runtime-flow-inspector =
+                pythoneda-tools-runtime-flow-inspector.packages.${system}.pythoneda-tools-runtime-flow-inspector-python312;
+            };
+          pythoneda-tools-runtime-flow-inspector-infrastructure-python313 =
+            pythoneda-tools-runtime-flow-inspector-infrastructure-for {
+              python = pkgs.python313;
+              pythoneda-shared-pythonlang-banner =
+                pythoneda-shared-pythonlang-banner.packages.${system}.pythoneda-shared-pythonlang-banner-python313;
+              pythoneda-shared-pythonlang-domain =
+                pythoneda-shared-pythonlang-domain.packages.${system}.pythoneda-shared-pythonlang-domain-python313;
+              pythoneda-shared-pythonlang-infrastructure =
+                pythoneda-shared-pythonlang-infrastructure.packages.${system}.pythoneda-shared-pythonlang-infrastructure-python313;
+              pythoneda-tools-runtime-flow-inspector =
+                pythoneda-tools-runtime-flow-inspector.packages.${system}.pythoneda-tools-runtime-flow-inspector-python313;
+            };
+        };
+      });
+}
