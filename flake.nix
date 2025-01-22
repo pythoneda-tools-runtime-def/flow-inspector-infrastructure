2cacce5caf96057174780b85c2f7564a2f0c8337
rydnr <github@acm-sl.org>
2025-01-22 20:31:35 +0100

    Updated nixpkgs to 24.11 and fixed wheel

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
   description = "Nix flake for pythoneda-tools-runtime/flow-inspector-infrastructure";
   inputs = rec {
     flake-utils.url = "github:numtide/flake-utils/v1.0.0";
-    nixpkgs.url = "github:NixOS/nixpkgs/24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
     pythoneda-shared-pythonlang-banner = {
       inputs.flake-utils.follows = "flake-utils";
       inputs.nixpkgs.follows = "nixpkgs";
